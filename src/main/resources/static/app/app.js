7f9416cf5989d67c9b2164798e043b1d74d373d2
Prasad <renuka.prasad@rakuten.com>
2017-11-15 17:43:14 +0900

    Route configuration

--- src/main/resources/static/app/app.js
+++ src/main/resources/static/app/app.js
@@ -1,1 +1,16 @@
-var app = angular.module('crudApp', []);
+var app = angular.module('crudApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: '/templates/home.html'
+    }).when('/register', {
+        templateUrl: '/templates/register.html',
+        controller: 'UserCRUDController'
+    }).when('/login', {
+        templateUrl: '/templates/login.html'
+    }).when('/resumeForm', {
+        templateUrl: '/templates/resumeForm.html'
+    }).otherwise({
+        redirectTo: '/'
+    });
+});
